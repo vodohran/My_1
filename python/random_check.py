668d674dfdb46e1142b47494ef352f01bc47d9c6
vodohran <shmeihel@gmail.com>
2021-08-30 19:17:13 +0300

    proverka_test

--- python/random_check.py
+++ python/random_check.py
@@ -1,5 +1,10 @@
 import random
-a = random.randint (0,99)
-for i in range(5):
-    a = random.randint(0,99)
-    print('Random digit = '+str(a))
+count = 10000
+check_count = 0
+for i in range(count):
+    a = random.randint(0,9)
+    b = random.randint(0,9)
+    if a==b: check_count +=1
+
+print('Veroyatnost = ')
+print(check_count*100/count)
