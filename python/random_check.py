07ec9e5e0e7e012d60be5ca0a4ad5dbe4cd7d314
vodohran <shmeihel@gmail.com>
2021-09-01 23:31:42 +0300

    ne pomnu

--- python/random_check.py
+++ python/random_check.py
@@ -1,10 +1,11 @@
 import random
 count = 10000
 check_count = 0
+
 for i in range(count):
-    a = random.randint(0,9)
-    b = random.randint(0,9)
-    if a==b: check_count +=1
+    a = random.randint(0,99)
+    b = random.randint(0,99)
+    if a>b: check_count +=1
 
 print('Veroyatnost = ')
-print(check_count*100/count)
+print(check_count/count)
